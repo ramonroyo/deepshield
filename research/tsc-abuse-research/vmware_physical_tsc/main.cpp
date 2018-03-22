e98bc6d6c39c856c3aa4c957b324e416cb4da3ce
Sherab Giovannini <sherab.giovannini@byteheed.com>
2018-03-22 15:55:33 +0100

    Added experimental measure tool for researching purposes.
    vmware_physical_tsc:
    
    - Detects if its running under VMware enviroment
    
    if so:
       - Discovers differences between physical TSC and virtualized RDTSC.
       - Uses functionality described here: https://www.vmware.com/pdf/vmware_timekeeping.pdf
    
    else:
       - Just shows native RDTSCP timmings.

---
+++ research/tsc-abuse-research/vmware_physical_tsc/main.cpp
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <intrin.h>
+#include <cstdint>
+#include <windows.h>
+
+constexpr int BLOCK_SIZE = 256;
+
+#define set_process_affinity(n) SetProcessAffinityMask(GetCurrentProcess(), n)
+
+uint64_t timming_rdtsc[BLOCK_SIZE];
+uint64_t timming_rdpmc_host_tsc[BLOCK_SIZE];
+uint64_t timming_rdpmc_elapsed_ns[BLOCK_SIZE];
+uint64_t timming_rdpmc_virtual_ns[BLOCK_SIZE];
+
+uint8_t probe[BLOCK_SIZE * 4096];
+
+extern "C" {
+  bool detect_vmware();
+  uint64_t memory_access_time(void *p);
+  uint64_t rdpmc_memory_access(void *p, unsigned int profile);
+}
+
+void dump_times(uint64_t * times, int size, char* tag) {
+    printf("############## %s ##############\n", tag);
+    for (int i = 0; i < size; ++i) {
+      if ((i + 1) % 16 == 0)
+        printf("% 6llu\n", times[i]);
+      else
+        printf("% 6llu", times[i]);
+    }
+    printf("##################################\n");
+}
+
+void timing_research(bool is_vmware) {
+  memset(probe, 0xfe, sizeof(probe));
+
+  for (int i = 0; i < 256; ++i)
+    _mm_clflush(probe + i * 4096);
+
+  // caching half of blocks
+  for (int i = 0; i < 256; i += 2)
+    uint64_t _ = memory_access_time(probe + i * 4096);
+
+  for (int i = 0; i < BLOCK_SIZE; ++i) {
+    void * address = probe + i * 4096;
+
+    if ( is_vmware ) {
+      timming_rdpmc_host_tsc[i]   = rdpmc_memory_access(address, 0x10000);
+      timming_rdpmc_elapsed_ns[i] = rdpmc_memory_access(address, 0x10001);
+      timming_rdpmc_virtual_ns[i] = rdpmc_memory_access(address, 0x10002);
+    }
+
+    timming_rdtsc[i] = memory_access_time(address);
+  }
+
+  if ( is_vmware ) {
+    dump_times(timming_rdpmc, BLOCK_SIZE, "RDPMC\0");
+  }
+
+  dump_times(timming_rdtsc, BLOCK_SIZE, "RDTSC\0");
+}
+
+int main(int argc, char *argv[]) {
+  UNREFERENCED_PARAMETER(argc);
+  UNREFERENCED_PARAMETER(argv);
+
+  bool is_vmware = false;
+
+  __try {
+      is_vmware = detect_vmware();
+  }
+    __except(EXCEPTION_EXECUTE_HANDLER) {
+      is_vmware = false;
+  }
+
+
+  if ( is_vmware ) {
+    printf("VMWARE is detected, using physical TSC.\n");
+  } else {
+    printf("VMWARE is NOT detected, using native TSC.\n");
+  }
+
+  system("pause");
+  set_process_affinity(1);
+  timing_research(is_vmware);
+  return 0;
+}
