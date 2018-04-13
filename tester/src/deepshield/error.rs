1dd94db9a806dd4d72819d7353291990f6efb21f
Sherab Giovannini <sherab.giovannini@byteheed.com>
2018-04-13 12:40:21 +0200

    Removed tester application (moved to deepshield-tester repository)

--- tester/src/deepshield/error.rs
+++
@@ -1,9 +0,0 @@
-use std::io::Error;
-
-#[derive(Fail, Debug)]
-pub enum DeepshieldError {
-    #[fail(display = "DeepShield I/O ({}) ({})", _0, _1)]
-    IoCall(String, #[cause] Error),
-    #[fail(display = "Error parsing: {} ({})", _0, _1)]
-    Parse(String, #[cause] Error),
-}
